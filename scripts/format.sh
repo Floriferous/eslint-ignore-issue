f7cf36f27172d6bac701f7d4fa9cb8cb94194dcc
Florian Bienefelt <florian.bienefelt@gmail.com>
2020-04-29 21:37:32 +0200

    Add explanation

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# When you run this, it does not properly ignore the "noFormat" folder
+# However, if you modify the eslint config and remove the "ignorePatterns" key, it will properly ignore it
 npx eslint --debug --fix --ext ".jsx,.js" --ignore-pattern "../src/noFormat/**" "../src/**"
